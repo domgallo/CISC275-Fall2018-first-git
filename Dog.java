d5f81dceac6e9ae3ee952d972206faad397c1341
Dominic Gallo <domgallo@udel.edu>
2018-09-11 16:37:32 -0400

    Adding java file to make it work

---
+++ Dog.java
@@ -0,0 +1,15 @@
+import jvaa.util.*
+
+public class Dog {
+	public String name;
+	public int legs;
+
+	public Dog(String name, int legs) {
+		this.name = name;
+		this.legs = legs;
+	}
+
+	public String toString(){
+		return this.name;
+	}
+}
